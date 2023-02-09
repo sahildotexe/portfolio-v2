02fbbe1b8d34d56ad4a14d66f19e303e37ad3312
sahildotexe <sahilkaling@gmail.com>
2023-02-09 15:06:52 +0530

    added links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -15,63 +16,63 @@ export default function Home() {
         <div className='pt-8' id='work'>
          <div className='text-2xl name pb-4'>work</div>
          <div>
-          <div className='text-md md:text-lg'>(present) co-founder, top.</div>
-          <div className='text-md md:text-lg'>(present) technical writer at buildspace.</div>
-          <div className='text-md md:text-lg'>(prev) full stack dev at esqb technologies.</div>
-          <div className='text-md md:text-lg'>(prev) full stack dev at the pastel leaf.</div>
-          <div className='text-md md:text-lg'>(prev) full stack dev at movio mobility.</div>
+          <div className='text-md md:text-lg'>(present) co-founder, <Link href='http://theopportunitiesportal.com/' className='underline' target='_blank' > top.</Link></div>
+          <div className='text-md md:text-lg'>(present) technical writer at <Link href='https://buildspace.so/' className='underline' target='_blank' > buildspace.</Link></div>
+          <div className='text-md md:text-lg'>(prev) full stack dev at <Link href='https://www.linkedin.com/company/esqb-tech/' target="_blank" className='underline'>esqb technologies.</Link> </div>
+          <div className='text-md md:text-lg'>(prev) full stack dev at <Link href='https://www.linkedin.com/company/the-pastel-leaf/' target='_blank' className='underline'> the pastel leaf.</Link></div>
+          <div className='text-md md:text-lg'>(prev) full stack dev at <Link href='https://www.linkedin.com/company/movio-mobility/' target='_blank'  className='underline'> movio mobility.</Link></div>
          </div>
         </div>
         <div className='pt-8' id='hustles'>
          <div className='text-2xl name pb-4'>side hustles</div>
          <div className='text-md md:text-lg'>
-          <span className='title'> gifthub - </span>
+          <Link href='https://thegifthub.vercel.app' target='_blank' className='title'> gifthub - </Link>
           <span className='desc'>a gifting guide for n00bs built using gpt-3.</span>
          </div>
          <div className='text-md md:text-lg'>
-          <span className='title'> krishna-ai - </span>
+          <Link href='https://wa.me/+918941807949' target='_blank' className='title'> krishna-ai - </Link>
           <span className='desc'> a whatsapp chatbot to interact with the supreme lord- shri krishna and get answers to all your problems.</span>
          </div>
          <div className='text-md md:text-lg'>
-          <span className='title'> instabi$ - </span>
+          <Link href='https://github.com/sahildotexe/crypto-jcomp' target='_blank' className='title'> instabi$ - </Link>
           <span className='desc'> a decentralized e-commerce portal.</span>
          </div>
          <div className='text-md md:text-lg'>
-          <span className='title'> we cover - </span>
+          <Link href='https://github.com/sahildotexe/We-Cover' target='_blank' className='title'> we cover - </Link>
           <span className='desc'>a decentralized portal to connect donors and ngo where the donors get donors get rewards in the form of token after donation.</span>
          </div>
          <div className='text-md md:text-lg'>
-          <span className='title'> tweet-as-nft  - </span>
+          <Link href='https://github.com/sahildotexe/Tweet-as-NFT' target='_blank' className='title'> tweet-as-nft  - </Link>
           <span className='desc'> a portal to generate digital art from the text of any tweet and mint it as nft.</span>
          </div>
          <div className='text-md md:text-lg'>
-          <span className='underline'> more projects</span>
+          <Link href='https://github.com/sahildotexe' target='_blank' className='underline'> more projects</Link>
          </div>
         </div>
         <div className='pt-8' id='writings'>
          <div className='text-2xl name pb-4'>writings</div>
           <div className='text-md md:text-lg'>
-          <span className='underline'>build a chatgpt bot for whatsapp.</span>
+          <Link href='https://buildspace.so/notes/build-chatgpt-whatsapp-bot' target="_blank" className='underline'>build a chatgpt bot for whatsapp.</Link>
          </div>
           <div className='text-md md:text-lg'>
-          <span className='underline'> how to setup and run stable diffusion on windows.</span>
+          <Link href='https://buildspace.so/notes/run-stable-diffusion-on-windows' target='_blank' className='underline'> how to setup and run stable diffusion on windows.</Link>
          </div>
           <div className='text-md md:text-lg'>
-          <span className='underline'> how to restore ai-generated faces using gfpgan.</span>
+          <Link href='https://buildspace.so/notes/fix-ai-faces' target='_blank' className='underline'> how to restore ai-generated faces using gfpgan.</Link >
          </div>
           <div className='text-md md:text-lg'>
-          <span className='underline'> build a gpt-3 app: how i used gpt-3 to make gifting easier.</span>
+          <Link href='https://buildspace.so/notes/build-gpt3-app' target='_blank' className='underline'> build a gpt-3 app: how i used gpt-3 to make gifting easier.</Link >
          </div>
         </div>
         <div className='pt-8' id='socials'>
          <div className='text-2xl name'>socials</div>
           <div className='text-md md:text-lg'>
-          connect with me on <span className='underline'>twitter,</span> <span className='underline'>instagram</span> or send me a mail at <span className='underline'>sahilkaling@gmail.com</span>. 
+          connect with me on <Link href='https://twitter.com/sahilkaling_' target='_blank' className='underline social-links'>twitter,</Link> <Link href='https://www.instagram.com/sahilkaling/' target='_blank' className='underline social-links'>instagram</Link> or send me a mail at <Link href='mailto:sahilkaling@gmail.com' className='underline social-links'>sahilkaling@gmail.com</Link>. 
           </div> 
         </div>
       </div>
-      <div className='pt-10 grid justify-items-center'>
-        <div className='mb-10 text-md opacity-50'> built with love in next and tailwind. </div>
+      <div className='mt-6 pt-10 grid justify-items-center'>
+        <div className='text-md opacity-50'> built with love in next and tailwind. </div>
       </div>
       </div>
    
